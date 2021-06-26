79ac4821d088349470a308e9bde79aea17d472c3
Rakha <rakhawibowo1998@gmail.com>
2021-06-26 16:32:26 +0700

    initial commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,13 @@ import bgHome from '../images/mobile/image-hero.jpg'
 const Home = () => {
     return (
         <div className="home">
-            <img className="bgHero" src={bgHome} alt="" />
+            {/* <img className="bgHero" src={bgHome} alt="" /> */}
+            <div className="overlay">
+
             <h1>
                 immersive experiences that deliver
             </h1>
+            </div>
         </div>
     )
 }
