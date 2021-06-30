8448cacae7e48691eb484f511ba5e996c3f44521
Rakha <rakhawibowo1998@gmail.com>
2021-06-30 11:24:17 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import About from './components/About';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import ComingSoon from './components/ComingSoon';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {HashRoute, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRoute>
       <div className="container">
         <Navbar />
         <Route path="/loopstudios-landing-page" exact component={About}/>
         <Route path="/coming-soon" exact component={ComingSoon}/>
         <Footer />
       </div>
-    </BrowserRouter>
+    </HashRoute>
   );
 }
 
